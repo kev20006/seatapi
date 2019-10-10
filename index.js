993cc5462a0f83588dcccde8d2b26727530de2de
Kevin Stewart <Kevin.Stewart@LC-LTM033.local>
2019-10-10 12:23:14 +0100

    first api end point and seatmap added

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const redisControls = require('./redisDemo');
+redisControls.redisInit()
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    redisControls.getKeys((key)=>{
+        res.json({"keys":key})
+        
+    })
+})
+
+app.get('/seatmap/:key', (req, res) => { 
+    res.setHeader('Content-Type', 'application/json')
+    redisControls.getValue(req.params.key, (key, result)=>{
+      res.json( JSON.parse(result) )
+    })
+})
+
+app.listen(1337, () => console.log(`Example app listening on port ${1337}!`))
---
+++ redisDemo.js
@@ -0,0 +1,43 @@
+const redis = require('redis');
+const client = redis.createClient();
+const  seatmap1 = require('./seatmaps/seatmap')
+
+
+
+
+client.on('connect', ()=>{
+    console.log( 'Redis Client connected' );
+})
+
+client.on('error', ()=>{
+    console.log( 'Something went wrong ' + err );
+})
+
+module.exports ={
+
+    redisInit: () => {
+        client.set("seatmap1", JSON.stringify(seatmap1))
+    },
+
+    setValue: (key, value) => {
+        client.set(key, value, redis.print)
+    },
+
+    getValue: (key, onResult) =>{
+        client.get(key, (error, result)=> {
+            if (error) {
+                console.log(error);
+                throw error;
+            }
+            onResult(key, result)
+        });
+    },
+
+    getKeys: (onKeys => {
+        client.keys('*', (err, keys) => {
+            if (err) return console.log(err);
+            onKeys(keys)
+        })
+    })
+}
+
